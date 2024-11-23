a34ea6a1c1d25b9cade5c0a7e8279aa41f06ba49
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-23 15:11:38 +0800

    feat: Superchat

--- src/zerolan/data/data/danmaku.py
+++ src/zerolan/data/data/danmaku.py
@@ -1,12 +1,27 @@
-from dataclasses import dataclass
+from pydantic import BaseModel
 
 
-@dataclass
-class Danmaku:
-    uid: str  # User id
-    username: str # Who send this danmaku
-    msg: str  # Message content
-    ts: int  # Timestamp
+class Danmaku(BaseModel):
+    """
+    Represents a danmaku entity from live-streaming.
 
-    def __str__(self):
-        return f'[{self.username}]({self.uid}): {self.msg}'
+    Attributes:
+        uid: The unique identifier of the user who sent this danmaku (depending on the platform).
+        username: The name or handle of the user who sent this danmaku.
+        content: The content message of the danmaku.
+        ts: The timestamp of when the danmaku was sent.
+    """
+    uid: str
+    username: str
+    content: str
+    ts: int
+
+
+class SuperChat(Danmaku):
+    """
+    Represents a Super Chat message from live-streaming.
+
+    Attributes:
+        money: The money sent by the user (depending on the platform).
+    """
+    money: str
